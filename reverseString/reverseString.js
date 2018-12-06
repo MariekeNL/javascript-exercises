1972b49feb7cdda58c006101780f5ddaf780c273
MariekeNL <mdezanger@gmail.com>
2018-12-06 17:28:46 +0100

    add code to js file

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+function reverseString(string) {
+    let splitString = string.split('');
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join('');
+    return joinArray;
 }
+
 
 module.exports = reverseString
