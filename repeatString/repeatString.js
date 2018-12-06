6d945231d3aa72bb443d03cc5e386eda10e11849
MariekeNL <mdezanger@gmail.com>
2018-12-06 17:07:26 +0100

    add code to pass tests in js file

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+function repeatString(string,amount) {
+    if (amount>=0) {
+        let newString ='';
+        for(let i = 0; i < amount; i++) {
+            newString = newString + string; 
+        }
+        return newString
+    } else {
+        return 'ERROR'
+    }
 }
 
 module.exports = repeatString
